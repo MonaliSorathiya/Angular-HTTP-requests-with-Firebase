95b7bc5709f5cfd4b9067e5cdfeb3c39a8830c85
Monali Sorathiya <monali.sorathiya@tensult.com>
2020-02-27 12:23:55 +0530

    Update UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sending-post-request';
+ 
+
+  constructor() {}
+
+  ngOnInit() {}
+
+
+
 }
