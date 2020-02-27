577f3e54096cf17d0049f522a23a0d11ebc0fa59
Monali Sorathiya <monali.sorathiya@tensult.com>
2020-02-27 15:54:43 +0530

    Update UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
- 
+export class AppComponent { 
 
   constructor() {}
 
   ngOnInit() {}
 
-
+  
 
 }
